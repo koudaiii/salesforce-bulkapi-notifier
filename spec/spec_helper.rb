a22a732ff7a6ff9357e5b58617128aec3e63ea16
koudaiii <cs006061@gmail.com>
2020-02-07 18:35:35 +0900

    Fix spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "salesforce-bulkapi-notifier"
+require "salesforce_bulkapi_notifier"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure

43aafb7917b95a7e9b2a8a79e4b2965617fa4ad2
koudaiii <cs006061@gmail.com>
2020-02-07 19:13:42 +0900

    Release 0.0.1

--- lib/salesforce_bulkapi_notifier/version.rb
+++ lib/salesforce_bulkapi_notifier/version.rb
@@ -1,3 +1,3 @@
 module SalesforceBulkAPINotifier
-  VERSION = '0.0.1.pre'
+  VERSION = '0.0.1'
 end
--- spec/salesforce_bulkapi_notifier.rb
+++ spec/salesforce_bulkapi_notifier.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe SalesforceBulkAPINotifier do
   it 'has a version number' do
-    expect(SalesforceBulkAPINotifier::VERSION).to eq('0.0.1.pre')
+    expect(SalesforceBulkAPINotifier::VERSION).to eq('0.0.1')
   end
 end
